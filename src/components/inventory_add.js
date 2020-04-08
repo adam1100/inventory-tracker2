e3c4bcd1876950c42b8e349783a0fd01cf77eab6
Abhay Khanka <58368777+abhayKRak3n@users.noreply.github.com>
2020-04-08 14:56:57 +0100

    Made changes to

--- src/components/inventory_add.js
+++ src/components/inventory_add.js
@@ -1,56 +1,88 @@
 import React, {Component} from 'react';
-//import InventoryTable from
-//import TableRow from
-//import _button from
 
-//import * as firebase from 'firebase';
+import InventoryTables from '../components/UserInventory/InventoryTables';
+import UserMenu from '../components/UserMenu/UserMenu';
+import Inventories from "./UserInventory/Inventories";
+
+
 
 class inventory_add extends Component {
     constructor() {
         super();
-        this.ItemID = {
-            Description: ''
+        this.state = {
+            productName: '',
+            id: '',
+            description: '',
+            qty: 0
         }
         this.submit = this.submit.bind(this);
         this.inputHandler = this.inputHandler.bind(this);
     }
 
-    inputHandler(e) {
+    inputHandler(e) {       //?
         this.setState({
             [e.target.name]: e.target.value
         })
     }
-
     submit(e) {
         e.preventDefault();
-        let storeDetails = {
-            ItemId: this.state.ItemId,
-            Description: this.state.Description
+        let inventoryDetails = {
+            productName: this.state.productName,
+            id: this.state.id,
+            description: this.state.description,
+            qty: this.state.qty
         }
-        //console.log(storeDetails)
-       // {
-        //    this.props.AddStoreRequest(storeDetails)
+
+       // let stock = {
+         //   productName: this.state.productName,
+         //   qty: this.state.qty
        // }
+        {this.props.AddInventory(inventoryDetails)}
+        console.log("inventoryDetails", inventoryDetails)
     }
 
     render() {
-        function addItem() {
+        return (
+            <div ><center>
+                <h1>Add New Inventory</h1>
+                <form onSubmit={this.submit} >
+                    <TextField
+                        id="Product ID"
+                        name="productName"
+                        value={this.state.productName}
+                        floatingLabelText="Product Name"
+                        onChange={this.inputHandler}
+                    /><br /><br />
 
-        }
+                    <TextField
+                        type="text"
+                        hintText="description"
+                        name="description"
+                        value={this.state.description}
+                        floatingLabelText="description"
+                        onChange={this.inputHandler}
+                    /><br /><br />
 
-        //BUTTON FOR ADDING Inventory - need to be implemented
-        addItem()
-        {
-            AlertIOS.prompt(
-                'Add Item',
-                null,
-                [
-                    {text: 'Cancel', onPress: () => console.log('cancel Pressed'), style: 'cancel'},
-                    {text: 'OK', onPress: (text) => this.itemsRef.push({title: text})},
-                ],
-            )
-        }
+
+                    <TextField
+                        type="text"
+                        hintText="company"
+                        name="company"
+                        value={this.state.company}
+                        floatingLabelText="company"
+                        onChange={this.inputHandler}
+                    /><br />
+                    <br />
+
+                    <RaisedButton type="submit" label="Add Product" primary={false} secondary={true} /> <br /><br />
+                </form>
+
+            </center>
+            </div>
+        );
+
     }
 
 
 }
+

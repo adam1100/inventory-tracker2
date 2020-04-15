cd9fefe6f5dddb6ed01d80e6cd3d0de7c3394995
Abhay Khanka <58368777+abhayKRak3n@users.noreply.github.com>
2020-04-15 18:34:57 +0100

    Added input forms, still need to connect them to DB

--- src/components/UserInventory/InventoryTable.js
+++ src/components/UserInventory/InventoryTable.js
@@ -3,20 +3,20 @@ import TableRow from './TableRow'
 
 const inventoryTable = (props) => {
 
-    const fields = props.tableFields
-    const tableHeadings = fields.map( field => {
-      return <th>{field}</th>
-    });
-    const inputForms = fields.map( field => {
-      return  <div className="pair--table">
+        const fields = props.tableFields
+        const tableHeadings = fields.map( field => {
+            return <th>{field}</th>
+        });
+        const inputForms = fields.map( field => {
+            return  <div className="pair--table">
                 <p>{field}</p><input name = {field} onChange={props.changeInput}/>
-              </div>
-    });
-    const tableContents = props.tableRows.map(row => {
-        return <TableRow fields = {fields}
-                         content = {row.column_entries}
-                         state = {row.state}/>
-    });
+            </div>
+        });
+        const tableContents = props.tableRows.map(row => {
+            return <TableRow fields = {fields}
+                             content = {row.column_entries}
+                             state = {row.state}/>
+        });
     
     return(
     <div>
@@ -27,14 +27,14 @@ const inventoryTable = (props) => {
 
     <table className="user-table">
         <thead>
-          {tableHeadings}
+        {tableHeadings}
         </thead>
         <tbody>
             {tableContents}
         </tbody>
     </table>
-    {inputForms}
-    <button onClick={props.add}className="menu-btn--table">Add Item</button>
+        {inputForms}
+        <button onClick={props.add}className="menu-btn--table">Add Item</button>
 
     </div>
     );
